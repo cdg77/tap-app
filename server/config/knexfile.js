b00c26cf9591690d4c1a03a05adca19a8f32af92
J. Allen Tucker <j.allentucker@gmail.com>
2014-07-08 15:28:57 -0700

    set staging and production connections to process.env.DATABASE_URL

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -22,33 +22,19 @@ module.exports = {
 
   staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'postgresql'
     }
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'postgresql'
     }
   }
 };
