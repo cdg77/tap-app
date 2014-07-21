e6bf9f8e40309a12d681ac836f699d31f2364ec5
Carlo De Gregorio <cdegregorio@gmail.com>
2014-07-21 15:47:21 -0700

    added twitter follow button

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -45,3 +45,21 @@ TapApp.FocusInputComponent = Ember.TextField.extend({
     this.$().focus();
   }.on('didInsertElement')
 });
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+(function(d, s, id) {
+  var js,fjs=d.getElementsByTagName(s)[0];
+  if(!d.getElementById(id)){
+    js = d.createElement(s);
+    js.id = id;
+    js.src='https://platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js,fjs);
+  }
+}(document,'script','twitter-wjs'));
