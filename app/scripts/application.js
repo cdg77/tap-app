89ad2cd3e17da0666597bc39aaa932996c6c207f
Whitney Young <wbyoung@fadingred.com>
2014-07-24 14:49:44 -0700

    Moved script loading out of template generation.

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -50,16 +50,27 @@ TapApp.FocusInputComponent = Ember.TextField.extend({
   }.on('didInsertElement')
 });
 
+// TODO: replace 0000000000000000 app id below
 // Facebook Button
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) { return; }
-  js = d.createElement(s); js.id = id;
-  js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0';
+  js = d.createElement(s);
+  js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0&appId=0000000000000000';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 // Twitter Button
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if(!d.getElementById(id)){
+    js = d.createElement(s);
+    js.id = id;
+    js.src='https://platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+}(document,'script','twitter-wjs'));
 
 // Google+ Button
 (function() {
