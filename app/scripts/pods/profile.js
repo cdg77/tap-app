fd5acc5effe70be26b9474f2673f9dc97d9cb335
J. Allen Tucker <j.allentucker@gmail.com>
2014-07-28 15:45:41 -0700

    working on adding user info to profile

--- app/scripts/pods/profile.js
+++ app/scripts/pods/profile.js
@@ -3,17 +3,26 @@
 module.exports = function(TapApp) {
 // authenticate any route
   TapApp.ProfileRoute = Ember.Route.extend(Ember.AdmitOne.AuthenticatedRouteMixin, {
-  });
-
-  TapApp.ProfileRoute = Ember.Route.extend({
     model: function() {
       // TODO: can we send { user: 'current' } instead? not yet. you'd need
       // some features to be added to admit-one.
-      return this.store.find('pour', { user: this.get('session.id') });
+      var promises = {
+        pours: this.store.find('pour', { user: this.get('session.id') }),
+        user: this.store.find('user', this.get('session.id'))
+      }
+      return new Ember.RSVP.hash(promises);
     }
   });
 
-  TapApp.ProfileController = Ember.ArrayController.extend({
+  TapApp.ProfileController = Ember.ObjectController.extend({
+    // lookupItemController: function(object) {
+    //   console.log(object);
+    //   if (object) {
+    //     return 'pour';
+    //   } else {
+    //     return 'user';
+    //   }
+    // },
     itemController: 'pour',
     sortProperties: ['timeOfPour'],
     sortAscending: false
