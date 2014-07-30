51828a02de6750a55e39a40d0b64e26873c1eecd
Carlo De Gregorio <cdegregorio@gmail.com>
2014-07-30 11:47:44 -0700

    working map and autofill using google api

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -11,6 +11,7 @@ TapApp.Router.map(function() {
   this.route('profile');
   this.route('addPour');
   this.route('editProfile');
+  this.route('mapToVenue');
 });
 
 require('./pods/autocomplete.js')(TapApp);
@@ -25,6 +26,7 @@ require('./pods/pour.js')(TapApp);
 require('./pods/profile.js')(TapApp);
 require('./pods/signup.js')(TapApp);
 require('./pods/user.js')(TapApp);
+require('./pods/map-to-venue.js')(TapApp);
 
 TapApp.ApplicationAdapter = DS.RESTAdapter.extend({
   namespace: 'api'
@@ -92,55 +94,74 @@ TapApp.FocusInputComponent = Ember.TextField.extend({
 })();
 
 // Google Map
-(function() {
+(function(google) {
+
   google.maps.event.addDomListener(window, 'load', function() {
-    // TODO: this timeout waits a second to ensure that the template has been
-    // rendered before displaying the map. eventually, this code will probably
-    // move elsewhere, so it shouldn't matter.
+    var mapOptions = {
+      center: new google.maps.LatLng(45.5312541,-122.6670392),
+      zoom: 12
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+    var input = document.getElementById('pac-input');
+    var types = document.getElementById('type-selector');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
 
     var infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-
-      service.getDetails(request, function(place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-          });
-
-      var mapOptions = {
-        center: new google.maps.LatLng(45.5312541,-122.6670392),
-        zoom: 12
-      };
-
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-        marker.setMap(map);
+    var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
+    });
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        return;
       }
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      marker.setIcon(/** @type {google.maps.Icon} */({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      var address = '';
+      /* jshint camelcase: false */
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+      /* jshint camelcase: true */
+
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+    });
+
+    // setTimeout(function() {
+    //   var map = new google.maps.Map(document.getElementById('map-canvas'),
+    //       mapOptions);
+    //   marker.setMap(map);
+    // }, 1000);
   });
-    setTimeout(function() {
-      var map = new google.maps.Map(document.getElementById('map-canvas'),
-          mapOptions);
-    }, 1000);
-  });
-})();
-
-// var infowindow = new google.maps.InfoWindow();
-//   var service = new google.maps.places.PlacesService(map);
-
-//   service.getDetails(request, function(place, status) {
-//     if (status == google.maps.places.PlacesServiceStatus.OK) {
-//       var marker = new google.maps.Marker({
-//         map: map,
-//         position: place.geometry.location
-//       });
-//       google.maps.event.addListener(marker, 'click', function() {
-//         infowindow.setContent(place.name);
-//         infowindow.open(map, this);
-//       });
-//     }
-//   });
-// }
-
-
+})(window.google);
 
--- app/scripts/pods/map-to-venue.js
+++ app/scripts/pods/map-to-venue.js
@@ -17,3 +17,4 @@ module.exports = function(TapApp) {
     sortAscending: false
   });
 };
+
